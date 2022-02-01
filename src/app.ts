bd6e35500a296bf71fab232b30797150f8656056
dunbar-brett <dunbar-brett>
2022-02-01 07:45:51 -0800

    more ormconfig set up, doesn't connect to db yet...

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express, { application } from 'express';
 import 'reflect-metadata';
+import express, { application } from 'express';
+import {createConnection} from "typeorm";
 
-const app = express();
-const port = 3000;
+createConnection().then(connection => {
+    const app = express();
+    const port = 3000;
+    app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hey buddy! Nodemon installed!!');
-});
-
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${3000}`);
-});
+    app.get('/', (req, res) => {
+        res.send('Hey buddy! Nodemon installed!!');
+    });
+    
+    app.listen(port, () => {
+        return console.log(`Express is listening at http://localhost:${3000}`);
+    });
+}).catch(error => console.log(error));
