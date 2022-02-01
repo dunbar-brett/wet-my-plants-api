96c20deca2f642fa6f14195947bf6cb237c8ce01
dunbar-brett <dunbar-brett>
2022-01-31 22:29:49 -0800

    express and typescript works, about to install nodemon

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { application } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hey buddy! Did this change???');
+});
+
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${3000}`);
+});
