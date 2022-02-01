8b6b5be62f5cc10412c750f0d1563470f3949e54
dunbar-brett <dunbar-brett>
2022-01-31 22:38:27 -0800

    installed nodemon

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hey buddy! Did this change???');
+    res.send('Hey buddy! Nodemon installed!!');
 });
 
 app.listen(port, () => {
