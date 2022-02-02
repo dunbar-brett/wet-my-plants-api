236682068ecacad38e194762bd7dd4d2e2da7349
dunbar-brett <dunbar-brett>
2022-02-02 08:17:42 -0800

    commented back in route code in app and index

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,36 @@
-// // import {Request, Response} from "express"; // do i need these?
-// import express from 'express';
-// import bodyParser from 'body-parser';
+import {Request, Response, NextFunction, Errback} from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Routes } from './routes';
+import { validationResult } from 'express-validator';
 
-// import {Routes} from "./routes";
-// import { validationResult } from "express-validator";
+// TODO: give error a type
+function handleError(err, _req: Request, res: Response, _next: NextFunction) {
+    res.status(err.statusCode || 500).send(err.message)
+}
+  
+const app = express();
+app.use(bodyParser.json());
 
-// function handleError(err, _req, res, _next) {
-//     res.status(err.statusCode || 500).send(err.message)
-//   }
-  
-//   const app = express();
-//   app.use(bodyParser.json());
-  
-//   Routes.forEach(route => {
-//     (app as any)[route.method](route.route,
-//       ...route.validation,
-//       async (req: Request, res: Response, next: Function) => {
-  
-//       try {
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//           return res.status(400).json({ errors: errors.array() });
-//         }
-  
-//         const result = await (new (route.controller as any))[route.action](req, res, next);
-//         res.json(result);
-//       } catch(err) {
-//         next(err);
-//       }
-//     });
-//   });
-  
-//   app.use(handleError);
-  
-//   export default app;
+Routes.forEach(route => {
+    (app as any)[route.method](route.route,
+        ...route.validation, // middleware validation
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({ errors: errors.array() });
+                }
 
-// TODO: comment this back out when routes are created
+                const result = await (new (route.controller as any))[route.action](req, res, next);
+                res.json(result);
+            } catch(err) {
+                next(err);
+            }
+        }
+    );
+});
+
+app.use(handleError);
+
+export default app;
