4a0413ffb9320b4cdddbb9931644d0041ddcbd04
dunbar-brett <dunbar-brett>
2022-02-01 22:58:51 -0800

    created start of routes

---
+++ src/routes.ts
@@ -0,0 +1,44 @@
+import {body, param} from 'express-validator';
+import {user} from './controller/user';
+
+export const Routes = [{
+    method: 'get',
+    route: '/users',
+    controller: user,
+    validation: []
+}];
+
+// EXAMPLE
+// export const Routes = [{
+//     method: "get",
+//     route: "/users",
+//     controller: UserController,
+//     action: "all",
+//     validation: [],
+//   }, {
+//     method: "get",
+//     route: "/users/:id",
+//     controller: UserController,
+//     action: "one",
+//     validation: [
+//       param('id').isInt(),
+//     ],
+//   }, {
+//     method: "post",
+//     route: "/users",
+//     controller: UserController,
+//     action: "save",
+//     validation: [
+//       body('firstName').isString(),
+//       body('lastName').isString(),
+//       body('age').isInt({ min: 0 }).withMessage('age must be a positive integer'),
+//     ],
+//   }, {
+//     method: "delete",
+//     route: "/users/:id",
+//     controller: UserController,
+//     action: "remove",
+//     validation: [
+//       param('id').isInt(),
+//     ],
+//   }];
