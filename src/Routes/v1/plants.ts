a45c1d1d1d18d63d9ec25812860c136fa20d1945
dunbar-brett <dunbar.brett@gmail.com>
2022-02-14 15:45:35 -0800

    added one more controller and route for plants

--- src/Routes/v1/plants.ts
+++ src/Routes/v1/plants.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 // just need to update this
 // import { list, register, update, remove } from '../../controller/plants';
-import { list, listById, register, update, remove } from '../../controller/plants/';
+import { list, listById, one, register, update, remove } from '../../controller/plants/';
 
 // import and install jwt
 // import checkRole
@@ -15,13 +15,14 @@ router.get('/', [], list);
 
 router.get('/all:id([0-9]+)', listById);
 
-router.get('/:id([0-9]+');
+router.get('/:id([0-9]+', one);
 
 router.post('/', [], register);
 
 router.patch('/:id([0-9]+)', // should this be put?
     [], 
-    update);
+    update
+);
 
 router.delete('/:id([0-9]+)', [], remove);
 
--- src/controller/plants/index.ts
+++ src/controller/plants/index.ts
@@ -1,5 +1,6 @@
 export * from './list';
 export * from './listById';
+export * from './one';
 export * from './register';
 export * from './remove';
 export * from './update';
