047d6e321f6541e3fccd9784ed308b4cd7171157
dunbar-brett <dunbar-brett>
2022-02-23 23:08:10 -0800

    i think i fixed the linter, who really knows

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 import page404 from './pages/404';
 import pageRoot from './pages/root';
 import v1 from './v1';
 
 const router = Router();
+const fruit = "apple";
 
 router.use('/v1', v1);
 router.use(pageRoot);
