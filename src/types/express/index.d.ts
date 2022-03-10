c5b28dd5c4cb9cb251d7987133e2b7de5d57c188
dunbar-brett <dunbar-brett>
2022-03-09 22:19:35 -0800

    installed jwt, added a bunch of middlewares for roles and jwt checks

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,14 +1,11 @@
-// import { Language } from 'typeorm/entities/users/types';
-
-// import { JwtPayload } from '../JwtPayload';
+import { JwtPayload } from '../JwtPayload';
 export {};
 
 declare global {
   namespace Express {
-    // export interface Request {
-    //   jwtPayload: JwtPayload;
-    //   language: Language;
-    // }
+    export interface Request {
+      jwtPayload: JwtPayload;
+    }
     export interface Response {
       customSuccess(httpStatusCode: number, message: string, data?: any): Response;
     }
